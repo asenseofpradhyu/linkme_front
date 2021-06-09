23a9fc2b40c29623e5a1ed6e716fdf80c6fd06a9
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2021-06-09 19:30:16 +0530

    Link Page design with dynamic add and remove links

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,31 @@
 // pages/_document.js
 
 import { ColorModeScript } from "@chakra-ui/react"
-import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import  NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import Document from "next/document"
+import { extractCritical } from "@emotion/server";
+import { resetServerContext } from "react-beautiful-dnd";
 import customTheme from "../theme/customTheme"
 
-export default class Document extends NextDocument {
+export default class MyDocument extends NextDocument {
+
+  static async getInitialProps(ctx) {
+    const page = await ctx.renderPage();
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(page.html);
+    resetServerContext();
+    return { ...initialProps, ...page, ...styles };
+  }
+
   render() {
     return (
       <Html lang="en">
-        <Head/>
+        <Head>
+        <style
+            data-emotion-css={this.props.ids.join(" ")}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
           <Main />
--- pages/admin/[name]/links.js
+++ pages/admin/[name]/links.js
@@ -1,8 +1,9 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
-import { FiChevronDown, FiChevronUp, FiRepeat, FiEdit2, FiCheck, FiToggleLeft, FiToggleRight, FiShuffle, FiImage, FiZap, FiClock, FiLayers, FiXCircle } from "react-icons/fi";
-import { Button, Spinner, Tag, Flex, Box, Spacer, Heading, Image, useDisclosure, useEditableControls, IconButton, Collapse, Tabs, TabList, TabPanels, Tab, TabPanel, Editable, EditableInput, EditablePreview ,Container, Center, FormControl, FormLabel, Input, Text, Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuIcon, MenuCommand, MenuDivider } from "@chakra-ui/react";
+import { DragDropContext, Droppable, Draggable, resetServerContext } from 'react-beautiful-dnd';
+import { FiChevronDown, FiChevronUp, FiRepeat, FiCopy, FiEdit2, FiCheck, FiBell, FiShuffle, FiImage, FiZap, FiClock, FiLayers, FiXCircle } from "react-icons/fi";
+import { Button, Spinner, Tag, Tooltip, useClipboard, Avatar, Link, Switch, Flex, Box, Spacer, Heading, Image, useDisclosure, useEditableControls, IconButton, Collapse, Tabs, TabList, TabPanels, Tab, TabPanel, Editable, EditableInput, EditablePreview ,Container, Center, FormControl, FormLabel, Input, Text, Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuIcon, MenuCommand, MenuDivider } from "@chakra-ui/react";
 
 // Local File Imports
 import { API_URL } from '../../../_helper/config';
@@ -13,10 +14,59 @@ const Links = () => {
 
     const router = useRouter();
     const { name } = router.query;
-    const { isOpen, onToggle } = useDisclosure();
-    console.log(isOpen);
-    const [analticsDropdown, setAnalticsDropdown] = useState('Weekly');
 
+    // Fake Data
+    const data = [
+      {
+        id: 1,
+        first_name: "Jeanette",
+        email: "jpenddreth0@census.gov",
+      },
+      {
+        id: 2,
+        first_name: "Giavani",
+        email: "gfrediani1@senate.gov",
+      },
+      {
+        id: 3,
+        first_name: "Noell",
+        email: "nbea2@imageshack.us",
+      },
+    ];
+
+    // States & Effects
+    const [analticsDropdown, setAnalticsDropdown] = useState('Weekly');
+    const [linkList, setlinkList] = useState({list:data});
+    const { isOpen, onToggle } = useDisclosure();
+    const { hasCopied, onCopy } = useClipboard(`https://www.linkwynk.com/${name}`);
+
+    
+    // Link List Drag
+    const onLinkDragEnd = (param) => {
+
+          if (!param.destination) return;
+
+          const items = Array.from(linkList.list);
+          const [reorderedItem] = items.splice(param.source.index, 1);
+          items.splice(param.destination.index, 0, reorderedItem);
+          setlinkList({list:items});
+          console.log(linkList);
+    };
+
+    // Add Link
+    const addLink = () => {
+      const obj = {'id':linkList.list.length+1, 'first_name':"Title", 'email':'url'};
+      linkList.list.unshift(obj);
+      setlinkList({list:linkList.list});
+      console.log(linkList);
+    };
+
+    // Remove Link
+    const removeLink = (link_id) => {
+      const removedLinklist = linkList.list.filter(item => item.id !== link_id);
+      setlinkList({list:removedLinklist});
+      console.log(linkList);
+    };
 
 
 
@@ -34,11 +84,11 @@ const Links = () => {
   
       return isEditing ? (
         <Box display="inline-block">
-          <IconButton height="auto" width="auto"  bg="transparent" _hover={ "background:transparent" } _active={ "background:transparent" } _focus={ "background:transparent" } icon={<FiCheck />} {...getSubmitButtonProps()} />
+          <IconButton height="auto" width="auto"  bg="transparent" _hover={{ backgroundColor:"none" }} _active={{ backgroundColor:"none" }} _focus={{ backgroundColor:"none" }} icon={<FiCheck />} {...getSubmitButtonProps()} />
         </Box>
       ) : (
         <Box display="inline-block">
-          <IconButton height="auto" width="auto" className="themeFont" bg="transparent" _hover={ "background:transparent" } _active={ "background:transparent" } _focus={ "background:transparent" } icon={<FiEdit2 />} {...getEditButtonProps()} />
+          <IconButton height="auto" width="auto" className="themeFont" bg="transparent" _hover={{ backgroundColor:"none" }} _active={{ backgroundColor:"none" }} _focus={{ backgroundColor:"none" }} icon={<FiEdit2 />} {...getEditButtonProps()} />
         </Box>
       )
     }
@@ -81,13 +131,13 @@ const Links = () => {
                       <Menu placement="bottom-end">
                           {({ isOpen }) => (
                             <>
-                              <MenuButton isActive={isOpen} as={Button} rightIcon={<FiChevronDown />} bg="#F5F5F7" fontSize="13px" _focus={"box-shahdow:none"}>
+                              <MenuButton isActive={isOpen} as={Button} rightIcon={<FiChevronDown />} bg="#F5F5F7" fontSize="13px"  _focus={{ boxShadow: "none" }}>
                                 { analticsDropdown }
                               </MenuButton>
                               <MenuList minWidth="120px">
-                                <MenuItem onClick={() => setAnalticsDropdown('Weekly')}>Weekly</MenuItem>
-                                <MenuItem onClick={() => setAnalticsDropdown('Monthly')}>Monthly</MenuItem>
-                                <MenuItem onClick={() => setAnalticsDropdown('Yearly')}>Yearly</MenuItem>
+                                <MenuItem _focus={{ background:"#0C0B0B", color:"#FFFFFF" }} onClick={() => setAnalticsDropdown('Weekly')}>Weekly</MenuItem>
+                                <MenuItem _focus={{ background:"#0C0B0B", color:"#FFFFFF" }} onClick={() => setAnalticsDropdown('Monthly')}>Monthly</MenuItem>
+                                <MenuItem _focus={{ background:"#0C0B0B", color:"#FFFFFF" }} onClick={() => setAnalticsDropdown('Yearly')}>Yearly</MenuItem>
                               </MenuList>
                             </>
                           )}
@@ -141,11 +191,29 @@ const Links = () => {
                   </Collapse>
             </Box>
         <Box className="add-links-section">
-            <Center marginTop="40px"><Button  className="theme-button add-link-btn" width="130px" padding="18px 40px" type="button">Add Links</Button></Center>
+            <Center marginTop="40px"><Button onClick={addLink} className="theme-button add-link-btn" width="130px" padding="18px 40px" type="button">Add Links</Button></Center>
+
+            {/* Start of Links Drag and Drop Section */}
+
             <Box className="draggable-container" marginTop="40px">
-                <Box className="links" bg="#F5F5F7" borderRadius="14px">
+
+            <DragDropContext  onDragEnd={onLinkDragEnd}>
+            <Droppable droppableId="droppable-1">
+
+            {(provided, _) => (
+              <Box  ref={provided.innerRef} {...provided.droppableProps}>
+
+{Object.values(linkList.list).map((type, i) => {
+          return (
+                <Draggable key={type.id}
+                draggableId={"draggable-" + type.id} index={i}>
+
+                {(provided, snapshot) => (
+                  <Box  ref={provided.innerRef} {...provided.draggableProps}>
+                    
+                <Box className="links" bg="#F5F5F7" borderRadius="14px" style={{ ...provided.draggableProps, boxShadow:snapshot.isDragging ? "0 0 .4rem #666" : "none"}}>
                   <Flex>
-                  <Box className="draggable-handle" padding="50px 16px" borderRight="2px solid rgba(12, 11, 11, 0.2)">
+                  <Box  {...provided.dragHandleProps} className="draggable-handle" padding="50px 16px" borderRight="2px solid rgba(12, 11, 11, 0.2)">
                   <Center transform="rotate(90deg)">
                     <FiRepeat fontSize="20px"/>
                   </Center>
@@ -155,7 +223,7 @@ const Links = () => {
                         <Box className="link-img" display="inline-block">
                         <Image
                               boxSize="64px"
-                              borderRight="12px"
+                              borderRadius="12px"
                               objectFit="cover"
                               src="https://bit.ly/dan-abramov"
                               alt="link-img"
@@ -163,20 +231,20 @@ const Links = () => {
                             />
                         </Box>
                         <Box className="link-detials" display="inline-block" marginLeft="22px">
-                        <Editable defaultValue="Title" isPreviewFocusable={false} className="editable-container">
+                        <Editable defaultValue={type.first_name} isPreviewFocusable={false} className="editable-container">
                           <EditablePreview className="themeFont" />
                           <EditableInput/>
                           <EditableControls />
                         </Editable>
-                        <Editable defaultValue="Url" isPreviewFocusable={false} className="editable-container">
+                        <Editable defaultValue={type.email} isPreviewFocusable={false} className="editable-container">
                           <EditablePreview />
                           <EditableInput/>
                           <EditableControls />
                         </Editable>
                         </Box>
                         <Box marginLeft="auto">
-                            <Box className="toggle-link" fontSize="40px" cursor="pointer">
-                              <FiToggleRight />
+                            <Box className="toggle-link">
+                            <Switch id="link-show-hide" size="md" />
                             </Box>
                         </Box>
                         </Flex>
@@ -197,7 +265,7 @@ const Links = () => {
                             <Box display="inline-block" marginRight="18px" fontSize="18px">
                               <FiLayers />
                             </Box>
-                            <Box display="inline-block" fontSize="18px" color="red">
+                            <Box display="inline-block" fontSize="18px" color="red" cursor="pointer" onClick={() => removeLink(type.id)}>
                               <FiXCircle />
                             </Box>
                           </Box>
@@ -205,74 +273,56 @@ const Links = () => {
                     </Box>
                   </Flex>
                 </Box>
-                <Box className="links" bg="#F5F5F7" borderRadius="14px">
-                  <Flex>
-                  <Box className="draggable-handle" padding="50px 16px" borderRight="2px solid rgba(12, 11, 11, 0.2)">
-                  <Center transform="rotate(90deg)">
-                    <FiRepeat fontSize="20px"/>
-                  </Center>
-                  </Box>
-                    <Box className="draggable-link-content" padding="8px 16px" width="100%">
-                        <Flex alignItems="center" className="edit-link-details">
-                        <Box className="link-img" display="inline-block">
-                        <Image
-                              boxSize="64px"
-                              borderRight="12px"
-                              objectFit="cover"
-                              src="https://bit.ly/dan-abramov"
-                              alt="link-img"
-                              // fallbackSrc="https://via.placeholder.com/150"
-                            />
-                        </Box>
-                        <Box className="link-detials" display="inline-block" marginLeft="22px">
-                        <Editable defaultValue="Title" isPreviewFocusable={false} className="editable-container">
-                          <EditablePreview className="themeFont" />
-                          <EditableInput/>
-                          <EditableControls />
-                        </Editable>
-                        <Editable defaultValue="Url" isPreviewFocusable={false} className="editable-container">
-                          <EditablePreview />
-                          <EditableInput/>
-                          <EditableControls />
-                        </Editable>
-                        </Box>
-                        <Box marginLeft="auto">
-                            <Box className="toggle-link" fontSize="40px" cursor="pointer">
-                              <FiToggleRight />
-                            </Box>
-                        </Box>
-                        </Flex>
-                        <Flex className="icon-links-control" marginTop="15px" justifyContent="flex-end">
-                          <Box>
-                            <Box display="inline-block" marginRight="18px" fontSize="18px">
-                              <FiShuffle  />
-                            </Box>
-                            <Box display="inline-block" marginRight="18px" fontSize="18px">
-                              <FiImage />
-                            </Box>
-                            <Box display="inline-block" marginRight="18px" fontSize="18px">
-                              <FiZap />
-                            </Box>
-                            <Box display="inline-block" marginRight="18px" fontSize="18px">
-                              <FiClock />
-                            </Box>
-                            <Box display="inline-block" marginRight="18px" fontSize="18px">
-                              <FiLayers />
-                            </Box>
-                            <Box display="inline-block" fontSize="18px" color="red">
-                              <FiXCircle />
-                            </Box>
-                          </Box>
-                        </Flex>
-                    </Box>
-                  </Flex>
+                
                 </Box>
+                  )}
+                </Draggable>
+                );
+              })}
+              {provided.placeholder}
+                </Box>
+                 )}
+                </Droppable>
+                </DragDropContext>
             </Box>
+
+            {/* End of Links Drag and Drop Section */}
         </Box>
        </Box>
     <Spacer />
-    <Box w="350px" h="100%" bg="red.500">
-      <Text>Preview</Text>
+    <Box w="400px" h="100%">
+      <Box className="link-preview-container" marginTop="31px">
+          <Box className="notification-profile-dropdown" display="flex" alignItems="center" justifyContent="flex-end">
+            <Box className="notification-bell" display="inline-block" boxSize="25px" marginRight="20px">
+            <FiBell fontSize="25px" />
+            </Box>
+            
+            <Box className="profile-dropdown" display="inline-block">
+            <Menu>
+              <MenuButton>
+                <Avatar name="User Image" boxSize="40px" borderRadius="8px" src="https://bit.ly/dan-abramov" />
+                <Box display="inline-block" marginTop="25%" marginLeft="5px"><FiChevronDown /></Box>
+              </MenuButton>
+              <MenuList minWidth="120px">
+                <MenuItem _focus={{ background:"#0C0B0B", color:"#FFFFFF" }}>Profile</MenuItem>
+                <MenuItem _focus={{ background:"#0C0B0B", color:"#FFFFFF" }}>Setting</MenuItem>
+                <MenuItem _focus={{ background:"#0C0B0B", color:"#FFFFFF" }}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+            </Box>
+          </Box>
+          <Box className="linkwynk-preview" marginTop="40px">
+                <Box className="mobile-design-frame" width="350px" height="calc(100vh - 25vh)"  border="solid #0C0B0B" borderWidth="8px 10px 8px 10px" borderRadius="40px">
+
+                </Box>
+          </Box>
+          <Box className="user-linkwynk-link" marginTop="30px" justifyContent="center" display="flex" alignItems="center">
+                <Text display="inline-block" fontWeight="700" fontSize="20px" marginRight="5px">My Linkwynk:</Text><Link fontSize="20px" href={"https://www.linkwynk.com/"+ name} isExternal>linkwynk.com/{ name }</Link>
+                <Box display="inline-block" marginLeft="5px" cursor="pointer" title="Copy" onClick={onCopy}>
+                {!hasCopied ? <FiCopy fontSize="22px"/> : <FiCheck ontSize="22px" />}
+                </Box>
+          </Box>
+      </Box>
     </Box>
   </Flex>
            </Box>
@@ -280,5 +330,6 @@ const Links = () => {
         </Box>
      );
 }
+
  
 export default Links;
